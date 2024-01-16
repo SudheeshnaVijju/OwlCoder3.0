78707e3f3b577dc6dec82b8ec3835e58ad0271cd
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2024-01-16 22:27:51 +0530

    Create Sequence of Sequence.cpp

---
+++ Sequence of Sequence.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+class Solution{
+public:
+    int numberSequence(int m, int n){
+        // code here
+        if(m<n) return 0;
+        if(n==0) return 1;
+        return numberSequence(m/2,n-1)+numberSequence(m-1,n);
+    }
+};
